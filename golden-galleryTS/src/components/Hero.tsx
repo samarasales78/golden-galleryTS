32e8ed330ff675b4ec55caa5843cec503f22e68c
samarasales78 <smatiassales@gmail.com>
2026-04-11 15:45:15 -0300

    implement responsive navbar component

---
+++ golden-galleryTS/src/components/Hero.tsx
@@ -0,0 +1,23 @@
+function Hero() {
+  return (
+    <section id="home" className="hero">
+      <div className="hero-wrapper">
+
+        <div className="hero-content">
+          <span className="hero-top">VISUAL DIARY</span>
+
+          <h1 className="hero-title">The Art of Life</h1>
+
+          <p className="hero-subtitle">Sketches</p>
+        </div>
+
+        <div className="hero-image">
+          <img src="/img/desenhoprincipal.jpeg" alt="Desenho principal" />
+        </div>
+
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ golden-galleryTS/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <header className="header">
+      <section>
+        <nav className="navbar">
+          <a href="#home" className="home">INÍCIO</a>
+          <a href="#desenhos">DESENHOS</a>
+          <a href="#sobre">SOBRE</a>
+          <a href="#contato">CONTATO</a>
+        </nav>
+      </section>
+    </header>
+  );
+}
+
+export default Navbar;
